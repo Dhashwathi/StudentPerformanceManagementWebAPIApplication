49b1e819675dca99b63aa847c58af85f0d7b3769
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-07-10 15:39:06 +0530

    ADD - Added get by id,put and delete methods

--- StudentPerformanceManagementWebApplication/Controllers/Services/StudentService.cs
+++ StudentPerformanceManagementWebApplication/Controllers/Services/StudentService.cs
@@ -2,18 +2,29 @@
 
 namespace StudentPerformanceManagementWebApplication.Controllers.Services
 {
-    public interface IStudentService
+    public interface IStudentService //Creating interface
     {
+        //Add method
         public Student AddStudent(AddUpdateStudent addUpdateStudentObj);
+        //get method
         public List<Student> GetAllStudents();
+        //Get student by Id
+        public Student GetStudentById(int StudentId);
+        //Update student By Id
+        public Student UpdateStudentById(int StudentId, AddUpdateStudent updStudObject);
+        //Delete method
+        public bool DeleteStudent(int StudentId);
     }
+    //Inheriting class class with interface
     public class StudentService : IStudentService
     {
+        //In memory List with tye student
         private readonly List<Student> StudentList;
-        public StudentService()
+        public StudentService() //Creating Constructor
         {
             StudentList = new List<Student>()
             {
+                //specifying initial values to properties
                 new Student()
                 {
                     StudentId=21001,
@@ -27,24 +38,69 @@ namespace StudentPerformanceManagementWebApplication.Controllers.Services
                 }
             };
         }
+        //Implementing Add method
         public Student AddStudent(AddUpdateStudent addUpdateStudentObj)
         {
             var addStud = new Student()
             {
                 StudentId = StudentList.Max(o => o.StudentId) + 1,
                 FullName = addUpdateStudentObj.FullName,
-                Department=addUpdateStudentObj.Department,
+                Department = addUpdateStudentObj.Department,
                 YearOfStudy = addUpdateStudentObj.YearOfStudy,
                 Marks = addUpdateStudentObj.Marks,
                 DateOfBirth = addUpdateStudentObj.DateOfBirth,
                 Email = addUpdateStudentObj.Email,
                 Gender = addUpdateStudentObj.Gender
             };
-            StudentList.Add(addStud);
+            StudentList.Add(addStud); //Added to list
             return addStud;
         }
-        public List<Student> GetAllStudents() {
-            return StudentList.ToList(); 
+        //Implementing Get method
+        public List<Student> GetAllStudents()
+        {
+            return StudentList.ToList();
         }
+
+        //implementing get by studentID
+        public Student GetStudentById(int InputStudentId)
+        {
+            var SelectStud = StudentList.FirstOrDefault(s => s.StudentId == InputStudentId);
+            return SelectStud;
+
+        }
+        //implementing Update Student 
+        public Student UpdateStudentById(int InputStudentId, AddUpdateStudent updateobj)
+        {
+            var selectStud = StudentList.FirstOrDefault(s => s.StudentId == InputStudentId);
+            if (selectStud != null)
+            {
+                selectStud.FullName = updateobj.FullName;
+                selectStud.Department = updateobj.Department;
+                selectStud.YearOfStudy = updateobj.YearOfStudy;
+                selectStud.Marks = updateobj.Marks;
+                selectStud.DateOfBirth = updateobj.DateOfBirth;
+                selectStud.Gender = updateobj.Gender;
+                selectStud.Email = updateobj.Email;
+                return selectStud;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        //implementing Delete student method
+        public bool DeleteStudent(int InputStudentId)
+        {
+            //Check the studentId exists
+            var removeStudent = StudentList.FirstOrDefault(o => o.StudentId == InputStudentId);
+            if (removeStudent != null)
+            {
+                StudentList.Remove(removeStudent);
+                return true;
+            }
+            return false;
+        }
+        
     }
 }
--- StudentPerformanceManagementWebApplication/Controllers/StudentController.cs
+++ StudentPerformanceManagementWebApplication/Controllers/StudentController.cs
@@ -15,7 +15,7 @@ namespace StudentPerformanceManagementWebApplication.Controllers
         {
             istudentService= studentServiceobj;
         }
-        [HttpPost]
+        [HttpPost] //Post Method
         public IActionResult AddingNewStudent(AddUpdateStudent addnewStud)
         {
             var newStud = istudentService.AddStudent(addnewStud);
@@ -24,11 +24,60 @@ namespace StudentPerformanceManagementWebApplication.Controllers
                 message = "success"
             });
         }
-        [HttpGet]
+        [HttpGet] //Get method
         public IActionResult GetStudentList()
         {
             var allStudents = istudentService.GetAllStudents();
             return Ok(allStudents);
+        }
+        //get by ID method
+        [HttpGet]
+        [Route("{StudId}")]
+        public IActionResult GetByStudentID(int StudId)
+        {
+            //call the seive method
+            var fetchByStudId= istudentService.GetStudentById(StudId);
+            if (fetchByStudId != null)
+            {
+                return Ok(fetchByStudId);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        //Udpate Method
+        [HttpPut]
+        public IActionResult UpdateMethodByStudentID(int StudID, AddUpdateStudent updStud)
+        {
+            var updstudbyid = istudentService.UpdateStudentById(StudID, updStud);
+            if (updstudbyid != null)
+            {
+                return Ok(updstudbyid);
+            }
+            else
+            {
+                return Ok(new
+                {
+                    message = "Invalid Student ID"
+                });
+            }
+
+        }
+        //Delete 
+        [HttpDelete]
+        public IActionResult DeleteByStudID(int StudID)
+        {
+            //Call the Service Delete method
+            var DeleteStudentByID = istudentService.DeleteStudent(StudID);
+            if(DeleteStudentByID== true)
+            {
+                return Ok(new
+                {
+                    message = "Student Deleted Successfully"
+                });
+            }
+            return NotFound();
         }
     }
 }
